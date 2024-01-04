801178219f740348e208d2922671bae639e97d6f
Akhilesh_kumar <86221348+Iamakhileshkumar@users.noreply.github.com>
2024-01-05 01:44:02 +0530

    other file added

---
+++ basics/01_variables.js
@@ -0,0 +1,31 @@
+const accountId=9907
+let accountemail="akhilesh@gmail.com"
+var accountPassword ="12345"
+accountCity="chandigarh"
+//const accountId=99 // not allowed
+console.log(accountId);
+console.table([accountId, accountPassword , accountemail,accountCity])
+/* 
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+/*
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+*/
---
+++ basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh" //string
+let age = 18 //number
+let isLoggedIn = true //
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  
+     //iska matlab    mein ne variable decalare krwaya pr value 
+//define nahi ki
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
