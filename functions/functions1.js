6b0d6b5fc0fb1c6e07afc88a60181972a69c1e5d
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-02 00:13:29 +0530

    chg

---
+++ functions/functions1.js
@@ -0,0 +1,18 @@
+function bolmeranaam (){
+    console.log("A");
+    console.log("k");
+    console.log("h");
+    console.log("i");
+    console.log("l");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+ // yeh hamar function h jismein the word function is function
+ //bolmera name defination h
+ //aur { iske ander  function ki defenation / funct
+//function ka scope h }
+bolmeranaam() // function run krwaya
+//bolmeranaam //yeh refernce h function ka
+ 
+
