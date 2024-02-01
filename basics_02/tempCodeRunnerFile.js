7a812e8f5bac37fc3d983f2cc30e473148801b22
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-01 21:15:14 +0530

    changes

--- basics_02/tempCodeRunnerFile.js
+++ basics_02/tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-marvel_heros.push(dc_heros)
-
-console.log(marvel_heros);
-console.log(marvel_heros[3][1]);
-
-const allHeros = marvel_heros.concat(dc_heros)
-console.log(allHeros);
+console.log(Object.keys(tinderUser));
