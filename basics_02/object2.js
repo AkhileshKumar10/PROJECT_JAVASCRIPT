9dc54ab59338ec41e177df3056175173bfc920db
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-01 21:15:07 +0530

    changes

--- basics_02/object2.js
+++ basics_02/object2.js
@@ -55,3 +55,34 @@ const obj2 ={1:"a", 4: "b"}
 
 const obj3 ={...obj1, ...obj2}
 console.log(obj3);
+
+
+
+//NOW FOR ARRAYS
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+// ] // array k ander bahut sare objects h unko ek bar loop throught krna h us
+//uske bare m uper batya h
+
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
