0fe894e07a63363bb3f388197ed8039d7ad13199
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-03 22:35:39 +0530

    changes

---
+++ functions/functions2.js
@@ -0,0 +1,6 @@
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+//... rest operator se baki k elements add hojayenge cart m
