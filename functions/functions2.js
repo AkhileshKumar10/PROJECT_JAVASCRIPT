c8c77ff974cad54aca8e11581eb06e2d1d26d601
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-03 22:45:03 +0530

    js

--- functions/functions2.js
+++ functions/functions2.js
@@ -4,3 +4,4 @@ function calculateCartPrice(val1, val2, ...num1){
 
 console.log(calculateCartPrice(200, 400, 500, 2000))
 //... rest operator se baki k elements add hojayenge cart m
+//... ka matlab h jo bhi items dikh rhe h use ek bundle m pack kro aur mujhe dedo
