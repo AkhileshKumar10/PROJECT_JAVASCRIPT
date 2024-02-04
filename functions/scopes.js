2e96819bdf92c604f3d726a5c016b619d2ed5475
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-04 11:52:07 +0530

    js

--- functions/scopes.js
+++ functions/scopes.js
@@ -3,6 +3,8 @@ let a =10
 const b = 20
 var c = 30
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// {} curly braces is scope in function
