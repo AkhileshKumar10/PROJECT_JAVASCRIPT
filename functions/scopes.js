aa1d89e05b5ee9b843f9fc8e3a2c4fb9edfcd762
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-04 12:33:06 +0530

    Refactor scopes.js file

--- functions/scopes.js
+++ functions/scopes.js
@@ -1,10 +1,20 @@
-//here we will talk about scpes
-let a =10
-const b = 20
-var c = 30
+// //here we will talk about scpes
+// // let a =10
+// // const b = 20
+// // var c = 30
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
 
-// {} curly braces is scope in function
+// // {} curly braces is scope in function
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+// //abh mein yahan varc ko console.log krunga toh vo 30 print karega
+
+// //jo bhi maine block k andar declare kiya h woh h block scope
+// //jo bhi maine block k bahar declare kiya h woh h global scope
