42ec43d1864ee4396c816cd325379432e81ed05b
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-04 11:37:56 +0530

    js

--- functions/functions2.js
+++ functions/functions2.js
@@ -5,3 +5,25 @@ function calculateCartPrice(val1, val2, ...num1){
 console.log(calculateCartPrice(200, 400, 500, 2000))
 //... rest operator se baki k elements add hojayenge cart m
 //... ka matlab h jo bhi items dikh rhe h use ek bundle m pack kro aur mujhe dedo
+
+
+const user ={
+    username : "hitesh",
+    price :199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is  ${anyobject.price}`);
+}
+
+//handleObject(user);
+handleObject({
+    username :"AKHILESH",
+    price : 999
+})
+
+//passing array
+const myNewArray =[200,400,100,6000]
+function returnsecondvalue(getArray){
+    return getArray[1]
+}
+console.log(returnsecondvalue(myNewArray));
