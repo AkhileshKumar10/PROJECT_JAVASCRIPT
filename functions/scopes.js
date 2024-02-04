c763b43550e12dee7570510e7a20b2bd1e217bec
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-04 14:41:37 +0530

    js

--- functions/scopes.js
+++ functions/scopes.js
@@ -30,6 +30,54 @@ if (true) {
 
 
 
+
+
+
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) //
+
+function addone(num){ //function declaration
+    return num + 1
+}
+
+
+
+addTwo(5) //error
+const addTwo = function(num){ //function expression
+    return num + 2
+}
