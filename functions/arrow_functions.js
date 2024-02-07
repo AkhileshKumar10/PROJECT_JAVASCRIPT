64f435d99631523017187b6de68bb0650c33a115
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-07 23:38:13 +0530

    Add arrow function for welcome message

--- functions/arrow_functions.js
+++ functions/arrow_functions.js
@@ -1,1 +1,10 @@
 //this keyword current context k bare m batata h
+const user ={
+    username: "hitesh",
+    price: 100,
+
+
+    welcomemessage: function(){
+        return "Welcome " + this.username
+    }
+}
