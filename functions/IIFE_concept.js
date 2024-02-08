d50d0debe481416aeaef55a23a3f1cde7b4708a6
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-08 23:49:30 +0530

    need

--- functions/IIFE_concept.js
+++ functions/IIFE_concept.js
@@ -2,5 +2,9 @@ function op (){
     console.log('db connected')
 }
 op()
+//need....?
 //kabhi kabhi yeh  problem function ati h ki globle scope 
 //ki wajh se cheeze pollute hojati h 
+//jese he function likha fatak se execute
+//fucntion k ander global variable koi problem kre
+//tabhi iife ka use kiya jata 
