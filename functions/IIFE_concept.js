675ceb89796e9e430803d312f5c0a1cc8b315fa8
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-08 23:54:57 +0530

    iife

--- functions/IIFE_concept.js
+++ functions/IIFE_concept.js
@@ -8,3 +8,17 @@ op()
 //jese he function likha fatak se execute
 //fucntion k ander global variable koi problem kre
 //tabhi iife ka use kiya jata 
+//jo bhi app fucntion m declare krhe h woh kai bar global scope pr affect krta h
+
+
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
