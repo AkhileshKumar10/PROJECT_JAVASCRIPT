4ea35cfeea1213bffd8e287302d5ca1965b35388
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-08 23:25:22 +0530

    changes

--- functions/arrow_functions.js
+++ functions/arrow_functions.js
@@ -70,3 +70,7 @@ console.log(addTwo(3, 4))
 // const myArray = [2, 5, 3, 7, 8]
 
 // myArray.forEach()
+
+
+//some important notes
+//js engine browser k ander hota tha global object tabhi window object hota h
