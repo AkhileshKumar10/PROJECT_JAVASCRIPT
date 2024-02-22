dbb817cff37abb9b77d77fe95bbfc1c9ba3faf94
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-02-22 22:03:28 +0530

    the js call stack

---
+++ js call stack/call_stack_in_js.js
@@ -0,0 +1,14 @@
+// //javascript execution content
+
+// 1.JS creates Global excution context 
+// 2.Next it creates memory phase
+// 3.Memory phase - In this phase,the variables are set to undefined 
+//   until the execution phase(next phase) and the functions are set to their definitions.
+// 4.Next it creates the execution phase
+// 5.Execution phase - In this phase,the variables are initialsed to given values and 
+//   when the function s are called,it creates a memory phase and execution phase for the function 
+//   just like the main one.
+
+// For live demo:
+// browser > inspect > Sources
+// create a snippet with few functions and visually experience the call stack.
