db01d46ea333b13184a21eff0eb225ba2648536b
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-03-02 10:51:45 +0530

    changes

---
+++ control flow/switch.js
@@ -0,0 +1,18 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+switch (month) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
