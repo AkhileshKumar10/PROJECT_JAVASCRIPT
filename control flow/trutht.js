2fd15b426236b67cb8b7c14aff59ce3756926d6f
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-03-02 11:45:28 +0530

    changes

--- control flow/trutht.js
+++ control flow/trutht.js
@@ -30,7 +30,7 @@ if (Object.keys(emptyObj).length === 0) {
 // Nullish Coalescing Operator (??): null undefined
 
 let val1;
-// val1 = 5 ?? 10
+// val1 = 5 ?? 10 //
 // val1 = null ?? 10
 // val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
@@ -38,6 +38,17 @@ val1 = null ?? 10 ?? 20
 
 
 console.log(val1);
+
+
+false==0 // retun true
+false =" "//returns true
+
+
+//terniary operator
+// condition ? true : false
+
+const iceTeaPrice=100;
+iceTeaPrice >= 80 ? console.log("less than 80")
 
 // Terniary Operator
 
