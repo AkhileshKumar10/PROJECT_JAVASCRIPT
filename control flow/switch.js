b9e26fa0e127854116e77b7fdabde30b4eb52644
Akhilesh Kumar <akhileshkumar101512@gmail.com>
2024-03-02 11:00:44 +0530

    changes

--- control flow/switch.js
+++ control flow/switch.js
@@ -12,7 +12,42 @@ switch (month) {
     case 1:
         console.log("Jan")
         break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("Mar")
+        break;
+    case 4:
+        console.log("Apr")
+        break;
+    case 5:
+        console.log("May")
+        break;
+    case 6:
+        console.log("Jun")
+        break;
+    case 7:
+        console.log("July")
+        break;
+    case 8:
+        console.log("August")
+        break;
+    case 9:
+        console.log("september")
+        break;
+
 
     default:
+        console.log("default case matched");
+        break;
+}
+
+
+//string k comparsion k liye
+const month = "march"
+switch (month){
+    case "jan":
+        console.log("January");
         break;
 }
